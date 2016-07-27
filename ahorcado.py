e6e3388da4c45a4ce0693a552881c957b8d8dad1
jose123456879 <totitosst@gmail.com>
2016-07-27 08:43:52 -0600

    jaja

--- ahorcado.py
+++ ahorcado.py
@@ -1,6 +1,10 @@
 from lista import lista_de_palabras
 import random
+print("Bienvenido a ahorcado")
+print("por favor ingresar un aletra a la vez")
+
 p= random.randrange(0,999)
+guiones=[]
 palabra=(lista_de_palabras(p))
-numero=count.palabra
-print((_)*numero)
+guiones=guiones(palabra)
+print(palabra)
---
+++ funciones.py
@@ -0,0 +1,13 @@
+def guiones(palabra):
+	for letra in palabra:
+		guiones.append("_")
+	return (guiones)	
+def encuentraletras(palabra,guiones):
+	for i in range(len(palabra)):
+		if palabra[i]=letra
+		
+
+
+
+
+  
